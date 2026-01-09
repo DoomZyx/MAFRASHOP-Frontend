715fa2aa4aa5d38f4428303fae0b69c12a4c4b3a
Axel Cella <160883113+DoomZyx@users.noreply.github.com>
2026-01-09 05:54:01 +0100

    fix: fix url in config.ts

--- src/API/config.ts
+++ src/API/config.ts
@@ -1,3 +1,3 @@
 // Configuration de l'API
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL ||  `${window.location.protocol}//${window.location.hostname}:8080`;;
+  import.meta.env.VITE_API_URL;
