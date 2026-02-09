3985f1d167e826ee22abfb9971384bbde583f348
Axel Cella <160883113+DoomZyx@users.noreply.github.com>
2026-02-09 14:12:37 +0100

    Add font-display: swap optimization for Bootstrap Icons fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import "./fonts-optimization.css";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
 import { WebSocketProvider } from "./contexts/WebSocketContext";
