f2c5d3f3de1d722ea2b5dc26e8359f44e722b4d1
Axel Cella <160883113+DoomZyx@users.noreply.github.com>
2026-02-12 14:44:55 +0100

    Suppression image du loader

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -3,8 +3,7 @@ import "./loader.scss";
 function Loader() {
   return (
    <div className="loader-container">
-    <img src="/images/race.webp" alt="" />
-  <span className="loader"></span>
+    <span className="loader"></span>
   </div>
   )
 }
